904c8c0418de6414feda31717d55f64ea995bdcf
MohammedOuttaleb <outaleb245@gmail.com>
2021-04-13 01:23:22 +0000

    building qcm logic ,calculating the score and the number of questions answered

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,16 +1,20 @@
+import { useState } from 'react';
 import './Question.css'
 
 function Question ({Setup}){
 
-    let index=0;
+    
+    const [score,setscore]=useState(0);
+    const [index,setindex]=useState(0);
+    const [questionnbr,setquestionnbr]=useState(0);
 
     const QuestionsList=[
         {
             body:"Who is the balon d'or winner for 2019?",
             type:"sports",
-            difficulty:"medium",
+            difficulty:"easy",
             responses:["Lionel Messi","Ronaldo","Neymar","Braithwaite"],
-            answer:1
+            answer:"Lionel Messi"
 
         },
         {
@@ -18,43 +22,74 @@ function Question ({Setup}){
             type:"sports",
             difficulty:"easy",
             responses:["R.louali","FERKOUS","Naciri","Hassan Elfadd"],
-            answer:1
+            answer:"R.louali"
 
         },
         {
             body:"Who is the best rapper for 2020 in Morroco?",
             type:"sports",
-            difficulty:"hard",
+            difficulty:"easy",
             responses:["SmalllX","El Grande ToTo","Dizzy Dros","Inkonnu"],
-            answer:1
+            answer:"SmalllX"
 
         },
         
 
     ];
     let finalList=[]; 
+     /*filltered list based on user parameters */ 
      QuestionsList.forEach(question => {
         if(   question.type.localeCompare(Setup.Category)===0 &&question.difficulty.localeCompare(Setup.Difficulty)===0 ){
             finalList.push(question);
         }
 
      });
+
     console.log(finalList);
 
+    //handler du nextquestion button 
     function ClickHandler(){
-        while (index<finalList.length()-1) {
-            
+        
+        if (index<=finalList.length-2) {
+
+         setindex(index+1);  
         }
+        else{
+            /** display game over popup */
+        }
+        setquestionnbr(questionnbr+1); 
+     }
+    //handler of answers buttons
+    function AnswerHandler(target){
+        setquestionnbr(questionnbr+1);
+      
+        if(target.value.localeCompare(finalList[index].answer)===0)  {
+            alert("right answer!!!");
+            setscore(score+1);
+            
+    
+           } 
+            else  alert("wrong answer!!!");
+
+        if(index<=finalList.length-2)  {
+         /* the game is not over*/ 
+        setindex(index+1);
+    }
+    else{
+        /* the game is over so we will display congratualation popup with the score*/
+        alert("game over!!!");
+    }
+
     }
 
     return(
         <div className="Qtemplate">
-            <label className="correct-answer">Correct Answers</label>
+            <label className="correct-answer">Correct Answers{score}/{questionnbr}</label>
             <h1>{finalList[index].body}</h1><br/><br/>
-            <button type="button" id="a1"  className="btn btn-outline-primary">{finalList[index].responses[0]}</button>
-            <button type="button" id="a2" className="btn btn-outline-primary">{finalList[index].responses[1]}</button>
-            <button type="button" id="a3" className="btn btn-outline-primary">{finalList[index].responses[2]}</button>
-            <button type="button"  id="a4" className="btn btn-outline-primary">{finalList[index].responses[3]}</button>
+            <button type="button" id="a1"  className="btn btn-outline-primary" value={finalList[index].responses[0]} onClick={ e => AnswerHandler(e.target)} >{finalList[index].responses[0]}</button>
+            <button type="button" id="a2" className="btn btn-outline-primary"  value={finalList[index].responses[1]} onClick={ e => AnswerHandler(e.target)} >{finalList[index].responses[1]}</button>
+            <button type="button" id="a3" className="btn btn-outline-primary"  value={finalList[index].responses[2]}   onClick={ e => AnswerHandler(e.target)} >{finalList[index].responses[2]}</button>
+            <button type="button"  id="a4" className="btn btn-outline-primary"  value={finalList[index].responses[3]}  onClick={ e => AnswerHandler(e.target)}>{finalList[index].responses[3]}</button>
             <button type="button" id="next" className="btn btn-outline-warning" onClick={ClickHandler}>Next Question</button>
 
         </div>
